2d321eb12ab330e4f7bf025485aa754bf0476daf
crappypapa <devsolaoluwa@gmail.com>
2021-06-24 14:31:27 +0100

    Add Library constructor

---
+++ main.js
@@ -0,0 +1,24 @@
+const myLibrary = [{
+  title: "Ruby",
+  author: "Microverse",
+  pages: 100,
+  status: true
+},  {
+  title: "Javascript",
+  author: "Odin",
+  pages: 150,
+  status: false
+
+}]
+
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+
+
+
