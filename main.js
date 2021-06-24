9f000ce9d60e86493042c0f15f5b79bde0e90a04
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-24 16:11:23 +0100

    Edit function

--- main.js
+++ main.js
@@ -11,12 +11,25 @@ const myLibrary = [{
 
 }]
 
-
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.status = status;
+}
+
+function addNewBook() {
+  document.querySelector('#book-form').addEventListener('submit', (e) => {
+    
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').value;
+    const book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+
+  });
 }
 
 
