7d7b17aad322c8c3aca1db0a4687644108d5a85b
crappypapa <devsolaoluwa@gmail.com>
2021-06-25 15:15:05 +0100

    Removed code comments

--- main.js
+++ main.js
@@ -56,24 +56,20 @@ function clearFields() {
 displayBooks()
 
 
- /* Event: Add a Book */
  document.querySelector('#book-form').addEventListener('submit', (e) => { 
 
   e.preventDefault();
 
-  //Get form Values
+
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const pages = document.querySelector('#pages').value;
   const read = document.querySelector('#read').value;
-  
-  // Instantiate Book
+
   const book = new Book (title, author, pages, read);
   
   console.log(book);
 
-
-  // Add Book to UI
   addBookToLibrary(book);
 
 
